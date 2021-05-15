fec80162f48644d62d1a78d5fff2d49a4b9ec8f3
Ragwolf <r4gwolf@gmail.com>
2021-05-15 12:36:15 +0100

    Disable frag effects for CS Gas and WP grenades

--- addons/ace_compat_sog_pf/CfgAmmo.hpp
+++ addons/ace_compat_sog_pf/CfgAmmo.hpp
@@ -2,8 +2,24 @@ class CfgAmmo
 {
 	class vn_grenadehand;
 
-	// Disable molotov frag effects
+	// Disable frag effects for Grenade (Molotov Cocktail)
 	class vn_molotov_grenade_ammo: vn_grenadehand
+	{
+		ace_frag_enabled = 0;
+		ace_frag_skip = 1;
+		ace_frag_force = 0;
+	};
+
+	// Disable frag effects for Grenade M7A3 (CS Gas)
+	class vn_m7_grenade_mag: vn_grenadehand
+	{
+		ace_frag_enabled = 0;
+		ace_frag_skip = 1;
+		ace_frag_force = 0;
+	};
+
+	// Disable frag effects for Grenade M34 (WP)
+	class vn_m34_grenade_mag: vn_grenadehand
 	{
 		ace_frag_enabled = 0;
 		ace_frag_skip = 1;
