42ff489a1c2891d7d9e77dde75d66a9f283e40b4
Ragwolf <r4gwolf@gmail.com>
2021-06-01 15:59:47 +0100

    Removes unnecessary units

--- addons/compat_sog_pf/CfgPatches.hpp
+++ addons/compat_sog_pf/CfgPatches.hpp
@@ -7,33 +7,11 @@ class CfgPatches
 
         requiredAddons[] =
         {
-            "ace_explosives", "vn"
+            "ace_explosives",
+            "vn"
         };
         requiredVersion = 0.1;
-        units[] =
-        {
-            "ACE_Explosives_Place_VN_Ammobox",
-            "ACE_Explosives_Place_VN_M112",
-            "ACE_Explosives_Place_VN_M14",
-            "ACE_Explosives_Place_VN_M15",
-            "ACE_Explosives_Place_VN_M16",
-            "ACE_Explosives_Place_VN_M18",
-            "ACE_Explosives_Place_VN_M18_Proximity",
-            "ACE_Explosives_Place_VN_M18_x3",
-            "ACE_Explosives_Place_VN_M18_Proximity_x3",
-            "ACE_Explosives_Place_VN_Punji_Large",
-            "ACE_Explosives_Place_VN_Punji_Small",
-            "ACE_Explosives_Place_VN_Punji_Whip",
-            "ACE_Explosives_Place_VN_Satchel_Charge",
-            "ACE_Explosives_Place_VN_TM57",
-            "ACE_Explosives_Place_VN_Trap_Arty",
-            "ACE_Explosives_Place_VN_Trap_F1_2m",
-            "ACE_Explosives_Place_VN_Trap_F1_4m",
-            "ACE_Explosives_Place_VN_Trap_M16_2m",
-            "ACE_Explosives_Place_VN_Trap_M16_4m",
-            "ACE_Explosives_Place_VN_Trap_M49_2m",
-            "ACE_Explosives_Place_VN_Trap_M49_4m"
-        };
+        units[] = {};
         weapons[] = {};
     };
 };
